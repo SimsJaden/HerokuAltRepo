01e6b04cd567c9edc7090bef5748615c2705d23c
Jaden Sims <jadensims@u.boisestate.edu>
2023-12-17 18:21:25 -0700

    Step 9 Test

--- src/main/java/com/example/heroku/HerokuApplication.java
+++ src/main/java/com/example/heroku/HerokuApplication.java
@@ -53,19 +53,19 @@ public class HerokuApplication {
     return "index";
   }
 
-  private String getRandomString() {
-    int leftLimit = 97; // letter 'a'
-    int rightLimit = 122; // letter 'z'
-    int targetStringLength = 10; // Length
-
-    Random random = new SecureRandom();
-    StringBuilder buffer = new StringBuilder(targetStringLength);
-    for (int i = 0; i < targetStringLength; i++) {
-      int randomLimitedInt = leftLimit + (int) (random.nextFloat() * (rightLimit - leftLimit + 1));
-      buffer.append((char) randomLimitedInt);
-    }
-    return buffer.toString();
-  }
+//  private String getRandomString() {
+//    int leftLimit = 97; // letter 'a'
+//    int rightLimit = 122; // letter 'z'
+//    int targetStringLength = 10; // Length
+//
+//    Random random = new SecureRandom();
+//    StringBuilder buffer = new StringBuilder(targetStringLength);
+//    for (int i = 0; i < targetStringLength; i++) {
+//      int randomLimitedInt = leftLimit + (int) (random.nextFloat() * (rightLimit - leftLimit + 1));
+//      buffer.append((char) randomLimitedInt);
+//    }
+//    return buffer.toString();
+//  }
 
   @RequestMapping("/db")
   String db(Map<String, Object> model) {
